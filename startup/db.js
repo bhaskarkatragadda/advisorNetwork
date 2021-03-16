94a913520d629d6c673303b5c1684589831398f3
bhaskarkatragadda <katragaddabhaskar1729@gmail.com>
2021-03-16 13:05:25 +0530

    initial commit project setup

---
+++ startup/db.js
@@ -0,0 +1,9 @@
+const winston=require('winston');
+const mongoose=require('mongoose');
+
+
+
+module.exports=function(){
+    mongoose.connect('mongodb://localhost:27017/advisor', {useNewUrlParser: true})
+    .then(()=>winston.info("connected to database"));
+}
