6a913fca1edfb89f50447bc05e80e8549308f84e
bhaskarkatragadda <katragaddabhaskar1729@gmail.com>
2021-03-19 23:00:51 +0530

    cleaned the code

--- routes/Users.js
+++ routes/Users.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const Advisor = require('../models/advisor');
 const Booking = require('../models/bookings');
-const  Mongoose  = require('mongoose');
 const validator = require('../middleware/validator');
 
 
@@ -13,7 +12,6 @@ router.post('/signin', async (req,res) =>{
                const userObj =  await User.findOne({email:req.body.email});
                if(userObj){
                    const isPassword = await userObj.authenticate(req.body.password);
-                   
                    if(isPassword){
                        let token=userObj.generatetoken();
                        return res.status(200).json({
@@ -26,6 +24,10 @@ router.post('/signin', async (req,res) =>{
                            messgage:"Password invalid"
                        })
                    }
+               }else{
+                   return res.status(400).json({
+                       message:"Invalid Email"
+                   })
                }
     }catch(e){
             return res.status(400).json({
@@ -36,31 +38,26 @@ router.post('/signin', async (req,res) =>{
 
 router.post('/register', async (req,res) =>{
     try{
-            let user_obj =  await User.findOne({email:req.body.email});
+            const { name, email, password } = req.body;
+            let user_obj =  await User.findOne({email:email});
             if(user_obj){
-                return res.status(400).json({
+                return res.status(409).json({
                     message:'User already registerd'
                 });
             }
-                // const { name, email, password } = req.body;
-                const hash_password = await bcrypt.hash(req.body.password,10);
-                const newUser = new User({ name:req.body.name, email:req.body.email,  hash_password: hash_password });
-                const result = await newUser.save();
-                if(result){
-                    let token=result.generatetoken();
-                    return res.status(200).json({
-                        messgage:'Successfully registered',
-                       JWT_token:token,
-                       userId:result._id
-                    });
-                }else{
-                    return res.status(400).json({
-                        messgage:'Something went wrong'
-                    });
-                }
-            
+            const hash_password = await bcrypt.hash(password,10);
+            const newUser = new User({ name:name, email:email,  hash_password: hash_password });
+            const result = await newUser.save();
+            if(result){
+                let token=result.generatetoken();
+                return res.status(200).json({
+                    messgage:'Successfully registered',
+                    JWT_token:token,
+                    userId:result._id
+                });
+            }   
     }catch(e){
-            return res.status(400).json({
+            return res.status(500).json({
                 messgage:'Something went wrong'
             });
     }
@@ -68,19 +65,27 @@ router.post('/register', async (req,res) =>{
 
 
 router.get('/:userId/advisor', async (req,res) => {
-    const advisor =  await Advisor.find();
-    if(advisor.length !=0){
-        return res.status(200).json({
-                advisor
-        });
-    }else{
-        return res.status(204).json({
-            message:"No advisors found"
-        });
+    try{
+        const advisor =  await Advisor.find();
+        if(advisor.length !=0){
+            return res.status(200).json({
+                    advisor
+            });
+        }else{
+            return res.status(404).json({
+                message:"No advisors found"
+            });
+        }
+    }catch(e){
+            return res.status(500).json({
+                message:"Somethin went wrong"
+            })
     }
+  
 });
 
 router.post('/:userId/advisor/:advisorId', async (req,res) => {
+    try{
         const userId = req.params.userId;
         const advisorId = req.params.advisorId;
         if(validator(userId) && validator(advisorId)){ 
@@ -94,32 +99,52 @@ router.post('/:userId/advisor/:advisorId', async (req,res) => {
                     })
                 } else{
                     return res.status(404).json({
-                        message:"UserId or AdvisorId are Invalid"
+                        message:"UserId or AdvisorId are not present"
                     })
                 }
+        }else{
+            return res.status(400).json({
+                message:"Invalid UserId or AdvisorId"
+            })
         }
+    }catch(e){
+            return res.status(500).json({
+                message:"Something went wrong"
+            });
+    }
+     
 });
 
 router.get('/:userId/advisor/booking', async (req,res) => {
-            const userId = req.params.userId;
-            if(validator(userId)){
-                const data = await Booking.find({userId:userId}).populate('advisorId');
-                console.log(data);
-                let arr = new Array();
-                data.forEach( async (element) =>{
-                   let a = {
-                       advisorId: element.advisorId._id,
-                       advisorName:element.advisorId.name,
-                       advisorProfilelUrl: element.advisorId.photoUrl,
-                       bookingId:element._id,
-                       bookingTime:element.bookingTime
-                   };
-                   arr.push(a);
-                })
-                return res.status(200).json({
-                    arr
-                })
-            }
+    try{
+        const userId = req.params.userId;
+        if(validator(userId)){
+            const data = await Booking.find({userId:userId}).populate('advisorId');
+            let Bookings = new Array();
+            data.forEach( async (element) =>{
+               let bookingObj = {
+                   advisorId: element.advisorId._id,
+                   advisorName:element.advisorId.name,
+                   advisorProfilelUrl: element.advisorId.photoUrl,
+                   bookingId:element._id,
+                   bookingTime:element.bookingTime
+               };
+               arr.push(bookingObj);
+            })
+            return res.status(200).json({
+                Bookings
+            })
+        }else{
+            return res.status(400).json({
+                message:"Invalid UserId"
+            })
+        }
+    }catch(e){
+            return res.status(500).json({
+                message:"Something went wrong"
+            })
+    }
+            
 });
 
 
