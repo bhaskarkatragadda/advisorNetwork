f45b608d2586d11fd4365f06cf5180f235eaecd6
bhaskarkatragadda <katragaddabhaskar1729@gmail.com>
2021-03-20 16:35:40 +0530

    added landing api message

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRoutes = require('../routes/Users');
 const adminRoutes = require('../routes/admin');
+const Root = require('../routes/root');
 
 module.exports = function(app){
     app.use(express.json());
+    app.use(Root);
     app.use('/admin',adminRoutes);
     app.use('/user',userRoutes);
 }
